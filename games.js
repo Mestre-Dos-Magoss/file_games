5eb8c9f4bd46f83d5ae3fd18ac6ea9c6f2326a3e
Mestre dos Magos <ferminoapolinariojuliano@gmail.com>
2026-02-09 15:49:01 -0300

    tests...

--- games.js
+++ games.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
     res.status(200).json({
         jogos: [
         {
